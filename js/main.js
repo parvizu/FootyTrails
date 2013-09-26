d1f01ebc0348abc3042c832f8e65ffef2fed860a
Ramit Malhotra <ramitmalhotra@gmail.com>
2013-09-25 20:22:13 -0700

    JS file with player info extracted

--- js/main.js
+++ js/main.js
@@ -1,32 +1,5 @@
 // JavaScript Document
 
-function getStuff()
-{
-	$.ajax({
-			url:'https://sirgalahad88:pablopablo@api.delicious.com/v1/posts/get',
-			dataType: 'jsonp',
-			success: function(data) 
-			{
-				//console.log(data);
-				data.forEach(function(entry)
-					{
-						console.log(entry.u)
-					});
-			}
-		});
-}
-
-var info = function(data)
-	{
-		$("#information").append("Length: "+ data.length);
-		
-		data.forEach(function(entry)
-			{
-				$("#information").append("<li>: "+ entry.a);
-				
-			});
-	}
-
 function getPlayerData(player)
 {
 	$.ajax({
@@ -43,14 +16,75 @@ function getPlayerData(player)
 			},
 		success: function(data) 
 			{
+			var last_name="";
+			var first_name="";
+			var current_team="";
+			var history={};
+			var jsonPlayer=new Object();
 				console.log(data.length);
 				data.forEach(function(entry)
 					{
-						console.log(entry)
+					
+					var i;
+						for(i=0;i<entry.t.length;i++){
+								if(entry.t[i].charAt(0)=="$"){
+										last_name=entry.t[i].split(" ",2)[1];
+										first_name=entry.t[i].split(" ",2)[0].substr(1);
+								}
+						}
+						
+						console.log(entry);
+						
 					});
+				data.forEach(function(entry){
+					for(var j=0;j<entry.t.length;j++){
+						ct=last_name+"-"+(data.length-1);
+						var lname=entry.t[j].split("-",2)[0];
+						var num=0;
+						var team_name="";
+						var team_url="";
+						if(ct==entry.t[j])
+							{
+							for(var i=0;i<entry.t.length;i++){
+								if(entry.t[i].charAt(0)=="#")
+									{
+									current_team=entry.t[i].substr(1);
+									}
+							}
+							}
+						if(lname==last_name)
+							{
+							num=entry.t[j].split("-",2)[1];
+							for(var i=0;i<entry.t.length;i++){
+								if(entry.t[i].charAt(0)=="#")
+									{
+									team_name=entry.t[i].substr(1);
+									console.log(team_name);
+									}
+							}
+							team_url=entry.u;
+							history[num]={
+										"t_name":team_name,
+										"url":team_url
+									};
+								
+							
+							}
+					}
+							
+					});
+				jsonPlayer={
+						"f_name":first_name,
+						"l_name":last_name,
+						"current_team":current_team,
+						"history":history
+							
+						};
+					displayPlayer(jsonPlayer);
 			}
 	});	
 }
+
 
 
 $(document).ready(function()
